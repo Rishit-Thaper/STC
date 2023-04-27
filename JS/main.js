feb58cff5b50cd574c2dd2995f7ba5164b2bd287
Rishit-Thaper <rishitthaper5329@gmail.com>
2023-04-27 10:41:23 +0530

    main

--- JS/main.js
+++ JS/main.js
@@ -8,3 +8,21 @@ function handleClick(pdfURL) {
 
     pdfSection.scrollIntoView({ behavior: 'smooth' });
 }
+
+  function submitForm(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+    
+    var form = document.querySelector('form');
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("POST", form.action, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Handle the successful response, if needed
+        console.log(xhr.responseText);
+      }
+    };
+    
+    xhr.send(formData);
+  }
